ee10da3256d5f0dd79ff6473eaf6cf7755a63909
ptvondroste <ptvondroste@gmail.com>
2016-11-03 13:09:18 -0400

    corrected validator commit 2

--- MovieStore.java
+++ MovieStore.java
@@ -10,7 +10,7 @@ public class MovieStore {
 		System.out.println("There are ten movies in this List");
 		Scanner scan = new Scanner(System.in);
 		String continues;
-
+		String category;
 		ArrayList<Movie> movieCllct = new ArrayList<Movie>(10);
 
 		movieCllct.add(new Movie("Days Of Thunder", "Drama"));
@@ -25,15 +25,22 @@ public class MovieStore {
 		movieCllct.add(new Movie("Cars", "Animated"));
 
 		do {
+			while (true){
 			System.out.println("What category are you interested in: Drama, SciFi, Horror or Animated");
-			String category = scan.next();
+			category = scan.next();
 			//(!(category.equals("Animated"))||!(category.equals("Drama"))||!(category.equals("SciFi"))||!(category.equals("Horror")))
 			
-					
-			while (!(category.equals("Animated"))){
+				if (category.equals("Animated")||category.equals("Animated")||category.equals("Drama")||category.equals("SciFi")||category.equals("Horror")){
+				break;
+				}
+				else{
 				System.out.println("inValid Entry");
-				category = scan.next();
-			}
+				
+				}
+				}
+				//System.out.println("inValid Entry");
+				//category = scan.next();
+				
 			for (Movie s : movieCllct) {
 				if (s.getCategory() != null && category.contains("Drama") && s.getCategory().contains("Drama")) {
 					System.out.println(s.getTitle());
@@ -52,10 +59,11 @@ public class MovieStore {
 			System.out.println("Continue? Y/N:");
 			continues = scan.next();
 		} while (continues.startsWith("y"));
-			if (continues.equalsIgnoreCase("N")) {
+			
+		if (continues.equalsIgnoreCase("N")) {
 			System.out.println("GOODBYE");
 			}
-
+				
 		scan.close();
 	}
 }
